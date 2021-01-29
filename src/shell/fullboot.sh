9cdd1e17adb5ac994b9264d802e3eb0ab3b8715c
Junior Criste <informaticode@gmail.com>
2021-01-29 15:44:17 -0300

    Permissions Test

--- src/shell/fullboot.sh
+++ src/shell/fullboot.sh
@@ -1,2 +1,2 @@
-umount /dev/sdb1 && mkfs.vfat /dev/sdb1 && dd if=bodhi.iso of=/dev/sdb1 status=progress && sync && umount /dev/sdb1
+umount /dev/sdb1 && mkfs.vfat -I /dev/sdb1 && dd if=/home/junior/bodhi.iso of=/dev/sdb1 status=progress && sync && umount /dev/sdb1
 
